2d306a842efe50ccdb175dbda1da9accda7a842a
Airlangga <airlanggakrishna@gmail.com>
2026-03-11 10:48:05 +0700

    feat: rating main

---
+++ rating/cmd/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/airlangga-hub/movie-app-microservices/rating/internal/controller/rating"
+	httphandler "github.com/airlangga-hub/movie-app-microservices/rating/internal/handler/http"
+	"github.com/airlangga-hub/movie-app-microservices/rating/internal/repository/memory"
+)
+
+func main() {
+	log.Println("Starting the rating service")
+
+	repo := memory.New()
+	ctrl := rating.New(repo)
+	h := httphandler.New(ctrl)
+
+	http.Handle("/rating", http.HandlerFunc(h.Handle))
+
+	if err := http.ListenAndServe(":8082", nil); err != nil {
+		panic(err)
+	}
+}
