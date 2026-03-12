afb21ae286d1297589d59384669d6b96c2169de2
Airlangga <airlanggakrishna@gmail.com>
2026-03-12 09:18:54 +0700

    feat: movie service controller

--- movie/internal/controller/movie/controller.go
+++ movie/internal/controller/movie/controller.go
@@ -5,6 +5,8 @@ import (
 	"errors"
 
 	metadatamodel "github.com/airlangga-hub/movie-app-microservices/metadata/pkg/model"
+	"github.com/airlangga-hub/movie-app-microservices/movie/internal/gateway"
+	"github.com/airlangga-hub/movie-app-microservices/movie/pkg/model"
 	ratingmodel "github.com/airlangga-hub/movie-app-microservices/rating/pkg/model"
 )
 
@@ -25,4 +27,24 @@ type Controller struct {
 
 func New(ratingGateway ratingGateway, metadataGateway metadataGateway) *Controller {
 	return &Controller{ratingGateway: ratingGateway, metadataGateway: metadataGateway}
+}
+
+func (c *Controller) Get(ctx context.Context, id string) (*model.MovieDetails, error) {
+	metadata, err := c.metadataGateway.Get(ctx, id)
+	if err != nil {
+		return nil, err
+	}
+
+	details := &model.MovieDetails{Metadata: *metadata}
+
+	rating, err := c.ratingGateway.GetAggregatedRating(ctx, ratingmodel.RecordTypeMovie, ratingmodel.RecordID(id))
+	if err != nil && !errors.Is(err, gateway.ErrNotFound) {
+		// Just proceed in this case, it's ok not to have ratings yet.
+	} else if err != nil {
+		return nil, err
+	} else {
+		details.Rating = &rating
+	}
+
+	return details, nil
 }
