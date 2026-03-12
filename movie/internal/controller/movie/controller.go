4e47f7a6364aa9d42df638ef1e041597bcbd9c07
Airlangga <airlanggakrishna@gmail.com>
2026-03-12 09:12:18 +0700

    chore: New func

--- movie/internal/controller/movie/controller.go
+++ movie/internal/controller/movie/controller.go
@@ -3,17 +3,26 @@ package movie
 import (
 	"context"
 	"errors"
-	
-	ratingmodel "github.com/airlangga-hub/movie-app-microservices/rating/pkg/model"
+
 	metadatamodel "github.com/airlangga-hub/movie-app-microservices/metadata/pkg/model"
+	ratingmodel "github.com/airlangga-hub/movie-app-microservices/rating/pkg/model"
 )
 
 var ErrNotFound = errors.New("movie metadata not found")
 
 type ratingGateway interface {
 	GetAggregatedRating(ctx context.Context, recordType ratingmodel.RecordType, recordID ratingmodel.RecordID) (float64, error)
-	PutRating(ctx context.Context, recordType ratingmodel.RecordType,  recordID ratingmodel.RecordID, rating *ratingmodel.Rating) error
+	PutRating(ctx context.Context, recordType ratingmodel.RecordType, recordID ratingmodel.RecordID, rating *ratingmodel.Rating) error
 }
 type metadataGateway interface {
 	Get(ctx context.Context, id string) (*metadatamodel.Metadata, error)
+}
+
+type Controller struct {
+	ratingGateway   ratingGateway
+	metadataGateway metadataGateway
+}
+
+func New(ratingGateway ratingGateway, metadataGateway metadataGateway) *Controller {
+	return &Controller{ratingGateway: ratingGateway, metadataGateway: metadataGateway}
 }
